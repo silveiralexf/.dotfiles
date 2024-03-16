893016e96a0aed2156da7835b2723c2a740112ec
Felipe Silveira <silveiralexf@gmail.com>
2024-03-16 21:23:16 +0000

    chore: refactor lsp config
    Former-commit-id: 8439001c4df87475970cce6b1e763e33b3970f91

---
+++ nvim/lua/lsp/config.lua
@@ -0,0 +1,44 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "bash",
+        "dockerfile",
+        "go",
+        "gomod",
+        "gosum",
+        "gowork",
+        "hcl",
+        "helm",
+        "html",
+        "java",
+        "javascript",
+        "json",
+        "json5",
+        "jsonc",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "terraform",
+        "tsx",
+        "typescript",
+        "vim",
+        "vue",
+        "yaml",
+      })
+    end,
+    vim.filetype.add({
+      extension = { rasi = "rasi" },
+      pattern = {
+        [".*/waybar/config"] = "jsonc",
+        [".*/mako/config"] = "dosini",
+        [".*/kitty/*.conf"] = "bash",
+        [".*/hypr/.*%.conf"] = "hyprlang",
+      },
+    }),
+  },
+}
--- nvim/lua/lsp/servers/yaml.lua
+++ nvim/lua/lsp/servers/yaml.lua
@@ -1,61 +1,11 @@
 return {
   {
-    "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      vim.list_extend(opts.ensure_installed, {
-        "bash",
-        "dockerfile",
-        "go",
-        "gomod",
-        "gosum",
-        "gowork",
-        "hcl",
-        "helm",
-        "html",
-        "java",
-        "javascript",
-        "json",
-        "json5",
-        "jsonc",
-        "lua",
-        "markdown",
-        "markdown_inline",
-        "python",
-        "query",
-        "regex",
-        "terraform",
-        "tsx",
-        "typescript",
-        "vim",
-        "yaml",
-      })
-    end,
-  },
-  {
     "neovim/nvim-lspconfig",
     opts = {
       codelens = {
         enabled = true,
       },
       servers = {
-        ---
-        --- JSON/YAML
-        ---
-        jsonls = {
-          -- lazy-load schemastore when needed
-          on_new_config = function(new_config)
-            new_config.settings.json.schemas = new_config.settings.json.schemas or {}
-            vim.list_extend(new_config.settings.json.schemas, require("schemastore").json.schemas())
-          end,
-          settings = {
-            json = {
-              format = {
-                enable = true,
-              },
-              validate = { enable = true },
-            },
-          },
-        },
         yamlls = {
           -- Have to add this for yamlls to understand that we support line folding
           capabilities = {
@@ -89,10 +39,6 @@ return {
             },
           },
         },
-        ---
-        --- Rust
-        ---
-        ruff_lsp = {},
       },
     },
   },
