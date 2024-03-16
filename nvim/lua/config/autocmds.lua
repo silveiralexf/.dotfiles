2fbf547d0f3facdee4e24707c4303996676a6da8
Felipe Silveira <silveiralexf@gmail.com>
2024-03-16 21:23:46 +0000

    feat: improve vue and typescript support
    Former-commit-id: d5bd2d709e995eac561653fd343ede1db5211b62

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -11,3 +11,25 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt_local.foldenable = false
   end,
 })
+
+-- Setup Vue/Volar without conflicting with TypeScript server
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup("LspAttachConflicts", { clear = true }),
+  desc = "Prevent tsserver and volar conflict",
+  callback = function(args)
+    if not (args.data and args.data.client_id) then
+      return
+    end
+
+    local active_clients = vim.lsp.get_clients()
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+
+    if client ~= nil and client.name == "volar" then
+      for _, c in ipairs(active_clients) do
+        if c.name == "tsserver" then
+          c.stop()
+        end
+      end
+    end
+  end,
+})
---
+++ nvim/lua/lsp/servers/vue.lua
@@ -0,0 +1,22 @@
+local util = require("lspconfig.util")
+
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        volar = {
+          filetypes = {
+            "javascript",
+            "javascriptreact",
+            "json",
+            "typescript",
+            "typescriptreact",
+            "vue",
+          },
+          root_dir = util.root_pattern("src/App.vue"),
+        },
+      },
+    },
+  },
+}
