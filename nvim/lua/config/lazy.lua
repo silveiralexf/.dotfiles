a1dd3e9d5b60d7fc5a09aedf618fcc57b3d738b7
Felipe Silveira <silveiralexf@gmail.com>
2024-03-17 00:58:54 +0000

    chore: add lspconfig to imports
    Former-commit-id: 71462a34a99c4d2609c19d21cf78bc35a6690bda

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -22,6 +22,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.vscode" },
     -- import/override with your plugins
     { import = "plugins" },
+    { import = "lsp" },
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
