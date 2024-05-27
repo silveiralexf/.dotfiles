224e8a25bd868329597adb1d94f128d9ba106c8e
Felipe Silveira <silveiralexf@gmail.com>
2024-05-27 14:27:06 +0100

    feat: improve lsp configs
    Former-commit-id: bff5def30402bf74acaa1598867ee224dc52fd65

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -20,6 +20,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.ui.mini-animate" },
     { import = "lazyvim.plugins.extras.vscode" },
+    { import = "lazyvim.plugins.extras.dap.core" },
     -- import/override with your plugins
     { import = "plugins" },
     { import = "lsp" },
