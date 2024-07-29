1883f7a16ff2f5dda894c7049e17eace759df4c3
Felipe Silveira <silveiralexf@gmail.com>
2024-07-29 05:31:52 +0100

    chore(terminal): replace kitty with wezterm

---
+++ wezterm/utils/platform.lua
@@ -0,0 +1,18 @@
+local wezterm = require('wezterm')
+
+local function is_found(str, pattern)
+   return string.find(str, pattern) ~= nil
+end
+
+local function platform()
+   local is_linux = is_found(wezterm.target_triple, 'linux')
+   local is_mac = is_found(wezterm.target_triple, 'apple')
+   local os = is_linux and 'linux' or is_mac and 'mac' or 'unknown'
+   return {
+      os = os,
+      is_linux = is_linux,
+      is_mac = is_mac,
+   }
+end
+
+return platform
