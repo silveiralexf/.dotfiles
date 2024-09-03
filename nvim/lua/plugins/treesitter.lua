5e1f5a80c82eea9e5b1da19d14e8928ef9b054ae
Felipe Silveira <silveiralexf@gmail.com>
2024-09-03 19:26:53 +0100

    test(nvim): treesitter tweaks to validate

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -21,6 +21,15 @@ return {
       auto_install = true,
       highlight = { enable = true },
       indent = { enable = true },
+      textobjects = {
+        move = {
+          enable = true,
+          goto_next_start = { [']f'] = '@function.outer', [']c'] = '@class.outer', [']a'] = '@parameter.inner' },
+          goto_next_end = { [']F'] = '@function.outer', [']C'] = '@class.outer', [']A'] = '@parameter.inner' },
+          goto_previous_start = { ['[f'] = '@function.outer', ['[c'] = '@class.outer', ['[a'] = '@parameter.inner' },
+          goto_previous_end = { ['[F'] = '@function.outer', ['[C'] = '@class.outer', ['[A'] = '@parameter.inner' },
+        },
+      },
       ensure_installed = {
         'bash',
         'c',
@@ -48,6 +57,7 @@ return {
         'query',
         'regex',
         'rust',
+        'svelte',
         'templ',
         'terraform',
         'toml',
@@ -58,9 +68,15 @@ return {
         'xml',
         'yaml',
       },
-      ---@class TSConfig
-      ---@type TSConfig
-      incremental_selection = {
+    },
+    ---@type TSConfig
+    ---@class TSConfig
+    config = function(_, opts)
+      if type(opts.ensure_installed) == 'table' then
+        opts.ensure_installed = LazyVim.dedup(opts.ensure_installed)
+      end
+      require('nvim-treesitter.configs').setup(opts)
+      opts.incremental_selection = {
         enable = true,
         keymaps = {
           init_selection = '<C-space>',
@@ -68,23 +84,7 @@ return {
           scope_incremental = false,
           node_decremental = '<bs>',
         },
-      },
-      textobjects = {
-        move = {
-          enable = true,
-          goto_next_start = { [']f'] = '@function.outer', [']c'] = '@class.outer', [']a'] = '@parameter.inner' },
-          goto_next_end = { [']F'] = '@function.outer', [']C'] = '@class.outer', [']A'] = '@parameter.inner' },
-          goto_previous_start = { ['[f'] = '@function.outer', ['[c'] = '@class.outer', ['[a'] = '@parameter.inner' },
-          goto_previous_end = { ['[F'] = '@function.outer', ['[C'] = '@class.outer', ['[A'] = '@parameter.inner' },
-        },
-      },
-    },
-    ---@class TSConfig
-    config = function(_, opts)
-      if type(opts.ensure_installed) == 'table' then
-        opts.ensure_installed = LazyVim.dedup(opts.ensure_installed) ---@diagnostic disable-line
-      end
-      require('nvim-treesitter.configs').setup(opts)
+      }
     end,
   },
 }
