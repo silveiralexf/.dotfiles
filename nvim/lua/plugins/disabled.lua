f893abcfc1c56c3a481aff1698eaa73a7c196027
Felipe Silveira <silveiralexf@gmail.com>
2024-09-18 18:51:33 +0100

    chore: clean-up unused, small tweaks, and bump deps

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -4,10 +4,6 @@ return {
     'akinsho/bufferline.nvim',
     enabled = false,
   },
-  -- {
-  --   "lukas-reineke/headlines.nvim",
-  --   enabled = false,
-  -- },
   {
     'DavidAnson/markdownlint',
     enabled = false,
