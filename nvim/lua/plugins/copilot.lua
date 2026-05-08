26f8c7c1cef6703ada40a0da10bc678ed36b8c82
Felipe Silveira <silveiralexf@gmail.com>
2026-05-08 13:37:34 +0100

    feat(nvim): add copilot plugin

---
+++ nvim/lua/plugins/copilot.lua
@@ -0,0 +1,6 @@
+--- Copilot: GitHub AI-powered code completion tool developed by GitHub
+return {
+  specs = {
+    { src = 'https://github.com/github/copilot.vim.git', name = 'copilot.nvim' },
+  },
+}
